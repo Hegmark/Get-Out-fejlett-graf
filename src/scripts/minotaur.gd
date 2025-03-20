783a52c8742219163dcb84c175f249cf79df1385
Hegedűs Márk <hegemark08@gmail.com>
2025-03-20 21:27:35 +0100

    walking fixed, maze still needs some love

---
+++ src/scripts/minotaur.gd
@@ -0,0 +1,3 @@
+extends CharacterBody3D
+
+@onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
---
+++ src/scripts/player.gd
@@ -0,0 +1,35 @@
+extends RigidBody3D
+
+var mouse_sensitivity := 0.001
+var twist_input := 0.0
+var pitch_input := 0.0
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _process(delta: float) -> void:
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("movement_left", "movement_right")
+	input.z = Input.get_axis("movement_forward", "movement_backward")
+	
+	apply_central_force($TwistPivot.basis * input * 1500.0 * delta)
+	
+	var aligned_force = $TwistPivot.basis * input
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	$TwistPivot.rotate_y(twist_input)
+	$TwistPivot/PitchPivot.rotate_x(pitch_input)
+	$TwistPivot/PitchPivot.rotation.x = clamp($TwistPivot/PitchPivot.rotation.x, 
+		deg_to_rad(-30), 
+		deg_to_rad(30)
+	)
+	twist_input = 0.0
+	pitch_input = 0.0
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			twist_input = - event.relative.x * mouse_sensitivity
+			pitch_input = - event.relative.y * mouse_sensitivity
